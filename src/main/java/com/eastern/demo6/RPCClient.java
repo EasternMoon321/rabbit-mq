71f61f222307024938a0437e2c5ee32676f45b7c
EasternMoon321 <cs18570643214.com>
2022-09-23 00:19:26 +0800

    rpc

--- src/main/java/com/eastern/demo6/RPCClient.java
+++ src/main/java/com/eastern/demo6/RPCClient.java
@@ -1,6 +1,7 @@
 package com.eastern.demo6;
 
 import com.eastern.utils.PropertiesUtil;
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -8,9 +9,10 @@ import com.rabbitmq.client.AMQP.BasicProperties;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
-
-
 
 /**
  * @Author chensheng13
@@ -19,29 +21,73 @@ import java.util.concurrent.TimeoutException;
  * @Version 1.0
  */
 @Slf4j
-public class RPCClient {
-    private final static String QUEUE_NAME = "rpc_queue";
+public class RPCClient implements AutoCloseable {
+    private final String QUEUE_NAME = "rpc_queue";
+    private final Connection connection;
+    private final Channel channel;
 
-    public static void main(String[] args) throws IOException, TimeoutException {
+    public  RPCClient() throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(PropertiesUtil.get("host"));
         factory.setUsername("admin");
         factory.setPassword("Miss1314!");
-        try (
-                Connection connection = factory.newConnection();
-                Channel channel = connection.createChannel();
-        ) {
-            String callbackQueueName = channel.queueDeclare().getQueue();
-            BasicProperties properties = new BasicProperties
-                    .Builder()
-                    .replyTo(callbackQueueName)
-                    .build();
+        connection = factory.newConnection();
+        channel = connection.createChannel();
+    }
 
-            String message = args == null ? "hello rpc" : String.join(" ", args);
-            channel.basicPublish("", QUEUE_NAME, properties, message.getBytes());
+    public static void main(String[] args) throws IOException, TimeoutException {
 
-            log.info(" [x] Sent '{}', callback '{}'", message, callbackQueueName);
+        try (RPCClient fibonacciRpc = new RPCClient()) {
+            for (int i = 0; i < 32; ++i) {
+                String i_str = Integer.toString(i);
+                log.info(" [x] Requesting fib({})", i_str);
+                String response = fibonacciRpc.call(i_str);
+                log.info(" [.] Got '{}'", response);
+            }
+        } catch (Exception e) {
+            log.error("create RPCClient failed", e);
         }
+
+    }
+
+    private String call(String message) throws IOException, ExecutionException, InterruptedException {
+        final String corrId = UUID.randomUUID().toString();
+        // 声明回调队列
+        String replyQueueName  = channel.queueDeclare().getQueue();
+        BasicProperties props = new BasicProperties()
+                .builder()
+                // 绑定消息关联的唯一id
+                .correlationId(corrId)
+                // 绑定回调队列到属性
+                .replyTo(replyQueueName)
+                .build();
+        // 发送消息到队列
+        channel.basicPublish("", QUEUE_NAME, props, message.getBytes());
+
+        // 声明回调任务线程
+        final CompletableFuture<String> response = new CompletableFuture<>();
+
+        // 消息回调任务
+        String ctag = channel.basicConsume(replyQueueName, true, (consumerTag, delivery) -> {
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                // 完成回调任务，唤醒回调任务线程
+                response.complete(new String(delivery.getBody()));
+            } else {
+                log.error("unknwon corrid :{}, corrent corrid :{}", delivery.getProperties().getCorrelationId(), corrId);
+            }
+        } , consumerTag -> {
+        });
+
+        // 阻塞，等待回到任务完成
+        String result = response.get();
+        // 摧毁回调任务消费者（关联的队列[默认队列]也被消费）
+        channel.basicCancel(ctag);
+        return result;
+    }
+
+    @Override
+    public void close() throws Exception {
+        connection.close();
 
     }
 }
--- src/main/java/com/eastern/demo6/RPCServer.java
+++ src/main/java/com/eastern/demo6/RPCServer.java
@@ -1,10 +1,8 @@
 package com.eastern.demo6;
 
 import com.eastern.utils.PropertiesUtil;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.*;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
@@ -15,6 +13,8 @@ import java.util.concurrent.TimeoutException;
  * @Date 2022/9/22 14:01
  * @Version 1.0
  */
+
+@Slf4j
 public class RPCServer {
 
     private final static String QUEUE_NAME = "rpc_queue";
@@ -26,9 +26,39 @@ public class RPCServer {
         factory.setPassword("Miss1314!");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-        DeliverCallback deliverCallback = (consumerTag, delivery)-> {
 
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        // 清除队列上消息
+//        channel.queuePurge(QUEUE_NAME);
+        channel.basicQos(1);
+        log.info(" [x] Awaiting RPC requests");
+
+        // 创建消费任务
+        DeliverCallback deliverCallback = (consumerTag, delivery)-> {
+            // 创建回调任务属性
+            AMQP.BasicProperties replyProps  = new AMQP.BasicProperties()
+                    .builder()
+                    // 获取队列绑定的关联id
+                    .correlationId(delivery.getProperties().getCorrelationId())
+                    .build();
+            String response = "";
+            try {
+                String message = new String(delivery.getBody());
+                int n = Integer.parseInt(message);
+                log.info(" [.] fib({})", message);
+                response += fib(n);
+            } catch (RuntimeException e) {
+                log.error(" [.] ", e);
+            } finally {
+                // 发送消息到回调任务队列
+                channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, response.getBytes());
+                // 应答本次消费
+                channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            }
         };
+
+        // 关联消费者和投递消费任务
+        channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> {});
 
     }
 
